bbbdc961d931559583096b3b1976dceb865363cc
saikiranmothe <saikiran.mothe@gmail.com>
2014-09-08 09:51:51 +0530

    README updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-README.rdoc
+#Python
+
+######
+
+######Python was created by Guido Van Rossum in the early 90’s. It is now one of the most popular languages in existence. I fell in love with Python for its syntactic clarity. It’s basically executable pseudocode.
+
+###Python learning curve depends how we learn things...
+
+### I think i need a week to complete python.
+
+```python
+		http://learnxinyminutes.com/docs/python/
+```
+
+[Learn Python in 10 minutes](http://learnxinyminutes.com/docs/python/)
+
--- helloworld/application.py
+++ helloworld/application.py
@@ -3,20 +3,6 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 def hello_world(request):
-    return Response('Hello world!')
-
-if __name__ == '__main__':
-    config = Configurator()
-    config.add_view(hello_world)
-    app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', 8080, app)
-    server.serve_forever()
-
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-
-def hello_world(request):
 		return Response('Hello world!')
 
 if __name__ == '__main__':
